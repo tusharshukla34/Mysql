e9fb81606ca59bdc367384964310b18e58691a5c
tusharshukla34 <tusharshukla880@gmail.com>
2025-08-23 16:21:40 +0530

    first commit

---
+++ day1.sql
@@ -0,0 +1,6 @@
+CREATE database CYBROM1;   # to create the outer structure
+use cybrom1;               # to use the database
+show engines;
+show tables;               # to represent the table within the database
+select * from ;
+
