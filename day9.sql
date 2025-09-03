879325d6ab4ca417049db9709e2dc62bba0e58c3
tusharshukla34 <tusharshukla880@gmail.com>
2025-09-03 13:24:31 +0530

    Concept of Entity Relationship Diagram

---
+++ day9.sql
@@ -0,0 +1,18 @@
+# indroduction of ER diagram (Entity Relationship Diagram)
+
+# Entity      ➡️  An entity in an ER diagram is a real-world object or concept that becomes a table in a database. 
+# Weak Entity ➡️  An attribute is a property or characteristic that describes an entity.
+
+# Attribute ----------------------------------------------------
+#  -Simple attribute         ➡️ A simple attribute is an attribute that cannot be divided further into smaller components
+#  -Key attribute            ➡️ A key attribute is an attribute that uniquely identifies an entity and is typically chosen as the primary key in a database table.
+#  -Multivalued attribute    ➡️ A multivalued attribute is an attribute that can store more than one value for a single entity
+#  -Derived attribute        ➡️ A derived attribute is an attribute whose value is calculated from other attributes and may change over time
+#  -Composite attribute      ➡️ A composite attribute is an attribute that can be divided into smaller sub-attributes, each representing a meaningful part of the whole
+
+#  Relationship cardinality -------------------------------------
+#  -One to One
+#  -One to Many
+#  -Many to One
+#  -Many to Many
+
