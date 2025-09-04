984e11217c9c589d1d26608e9a1040f497e5acfb
tusharshukla34 <tusharshukla880@gmail.com>
2025-09-04 12:52:34 +0530

    reverse engineering and data querify

---
+++ day10.sql
@@ -0,0 +1,9 @@
+use dummy;
+show tables;
+describe employees;
+describe payments;
+describe orderdetails;
+describe productlines;
+select * from customers;
+select * from customers where city = "Nantes";
+select customernumber,city,state from customers where city = "Nantes";
