c65968072f5549dfa15e0912682e6e212b883ca6
tusharshukla34 <tusharshukla880@gmail.com>
2025-09-05 17:12:27 +0530

    orderby | limit | offset | update

---
+++ day11.sql
@@ -0,0 +1,18 @@
+use dummy;
+select * from customers;
+# Sequential  # order by 
+
+# categorical data
+select  * from customers order by customername;
+
+# numerical data
+select customernumber,city,postalcode,creditlimit from customers order by creditlimit;
+
+#limit #offset
+
+select * from customers order by creditlimit desc limit 1 offset 4;
+select * from customers order by creditlimit desc limit 4 offset 2;
+
+#update
+
+update customers set phone = 123456 where customernumber = 103;
