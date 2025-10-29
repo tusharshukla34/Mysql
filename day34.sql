8ef88539d82b1fd55cf81a836b524cde7c24fcbd
tusharshukla34 <tusharshukla880@gmail.com>
2025-10-29 16:17:48 +0530

    Stored Procedure

---
+++ day34.sql
@@ -0,0 +1,35 @@
+use dummy;
+select * from customers;
+# stored procedure
+
+Delimiter //
+create procedure customerinfo()
+begin 
+select * from customers where country = "usa";
+end //
+Delimiter ;
+
+call customerinfo();
+
+Delimiter //
+create procedure customercredit()
+begin
+select * from customers where creditlimit > 180000 and country = "usa";
+end //
+Delimiter ;
+
+call customercredit()
+
+
+Delimiter //
+create procedure getofficebycountryss(in countryname varchar(255))
+begin
+select * from offices where country = countryname;
+end //
+Delimiter ;
+
+call getofficebycountryss('USA');
+
+drop procedure customerinfo;
+
+
