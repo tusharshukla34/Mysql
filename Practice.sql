bc656a32f7253435aeab75a10fd228abc5abf099
tusharshukla34 <tusharshukla880@gmail.com>
2025-11-10 18:40:25 +0530

    prac

--- Practice.sql
+++ Practice.sql
@@ -8,5 +8,7 @@ show tables;
 select * from pcustomer;
 select * from porders;
 
+SET sql_safe_updates = 0;
 
-
+update porders set status ='On hold' where ordernumber = 10100;
+alter table porders change orderDate orderdates date;
